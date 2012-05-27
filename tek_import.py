506de7550f8473121585406b984b9fab9cb0fbee
Andrew M. C. Dawes <dawes@pacificu.edu>
2012-05-27 07:08:36 -0700

    first working version
    code still needs cleanup, but this is the first version that works

---
+++ tek_import.py
@@ -0,0 +1,57 @@
+from veusz.plugins import *
+from numpy import genfromtxt
+
+class ImportPluginExample(ImportPlugin):
+    """An example plugin for reading a set of unformatted numbers
+    from a file."""
+
+    name = "Tektronix CSV plugin"
+    author = "Andrew M. C. Dawes"
+    description = "Import data columns from a Tektronix scope CSV file"
+
+    # Uncomment this line for the plugin to get its own tab
+    promote='Tektronix'
+
+    file_extensions = set(['.example'])
+
+    def __init__(self):
+        ImportPlugin.__init__(self)
+        self.fields = [
+            ImportFieldCombo("channels", descr="Number of channels to import", items=("1", "2", "3", "4"),
+                             editable=False, default="1")
+            ]
+
+    def OlddoImport(self, params):
+        """Actually import data
+        params is a ImportPluginParams object.
+        Return a list of ImportDataset1D, ImportDataset2D objects
+        """
+
+        f = params.openFileWithEncoding()
+        data = []
+        xvals = []
+        yvals = []
+        chans = int(params.field_results["channels"])
+        column_indeces = [3,4]
+        for line in f:
+            xvals.append(line.split(",")[3])
+            yvals.append(line.split(",")[4])
+        return [ImportDataset1D("ch1x", xvals),
+        ImportDataset1D("ch1y", yvals)]
+
+    def doImport(self, params):
+        """Actually import data
+        params is a ImportPluginParams object.
+        Return a list of ImportDataset1D, ImportDataset2D objects
+        """
+
+        f = params.openFileWithEncoding()
+        data = genfromtxt(f, delimiter=",")
+        chans = int(params.field_results["channels"])
+        column_indeces = [3,4]
+        return [ImportDataset1D("ch1x", data[:,3]),
+                ImportDataset1D("ch1y", data[:,4])]
+
+
+importpluginregistry.append(ImportPluginExample())
+
